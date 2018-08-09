f240a72e09a57a87c8e6e82e9c972698d5712c36
Alex Tharp <alex@toastercup.io>
2018-08-09 05:24:11 -0400

    fix(Docker Compose + Webpacker): clobber built packs upon launch

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+bin/rails webpacker:clobber
+
 if [ "$DEPLOYED" != "true" ]
 then
   bin/rails db:migrate 2>/dev/null || bin/rails db:setup cortex_starter:content_types:seed cortex:rebuild_indexes || true
